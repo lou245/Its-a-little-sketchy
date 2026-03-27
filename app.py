724ae65a548186d319dba3a78ea12c90204191c8
lou245 <lou@mediaideas.tv>
2026-03-27 10:47:26 -0500

    Configure app to run on specified port from environment

--- app.py
+++ app.py
@@ -41,5 +41,8 @@ def list_videos():
 def gallery():
     return app.send_static_file('gallery.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
