79aa2847836820342173d34c6194003be22610c0
lou245 <lou@mediaideas.tv>
2026-03-27 11:44:16 -0500

    Remove unnecessary blank lines in app.py

--- app.py
+++ app.py
@@ -40,9 +40,7 @@ def list_videos():
 @app.route('/gallery')
 def gallery():
     return app.send_static_file('gallery.html')
-
 import os
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
